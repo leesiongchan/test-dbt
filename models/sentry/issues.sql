a2c0b5011d4948f7db234ccfdabd4a12bd08cc01
Harvey <leesiongchan@duck.com>
2023-11-30 10:52:03 +0800

    Update

--- models/sentry/issues.sql
+++ models/sentry/issues.sql
@@ -1,10 +1,10 @@
 WITH
 issues AS (
     SELECT
-        group_id AS issue_id,
+        group_id AS id,
         COUNT(report_type) AS total_errors
     FROM {{ ref('events') }}
-    GROUP BY issue_id
+    GROUP BY id
 )
 
 SELECT *
