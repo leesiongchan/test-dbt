da599d3b24822d888d0055e64dedd809d80eb1da
Harvey <leesiongchan@duck.com>
2023-12-27 14:48:51 +0800

    Update

---
+++ models/sentry/extension_events.sql
@@ -0,0 +1,20 @@
+WITH sentry_events AS (
+    SELECT
+        datecreated AS date_created,
+        id,
+        culprit,
+        groupid AS group_id,
+        projectid AS project_id,
+        tags,
+        title
+    FROM {{ ref('airbyte_events') }}
+),
+cet_sentry_events AS (
+    SELECT
+        e.*,
+        et.value::TEXT AS report_type
+    FROM sentry_events e, e.tags AS et
+    WHERE et.key = 'ReportType'
+)
+
+SELECT * FROM cet_sentry_events
