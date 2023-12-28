7f6d08008696efb398f8e6ec55b29147639daefd
Harvey <leesiongchan@duck.com>
2023-12-28 12:03:41 +0800

    Update

--- models/sentry/extension_events.sql
+++ models/sentry/extension_events.sql
@@ -6,8 +6,12 @@ WITH sentry_events AS (
         groupid AS group_id,
         projectid AS project_id,
         tags,
-        title
+        title,
     FROM {{ source('airbyte', 'airbyte_events') }}
+    {% if is_incremental() %}
+    WHERE _airbyte_emitted_at > (SELECT MAX(_airbyte_emitted_at) FROM {{ this }})
+    {% endif %}
+
 ),
 cet_sentry_events AS (
     SELECT
